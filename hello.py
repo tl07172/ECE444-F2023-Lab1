7c931f5a70c3b4dcdee45dee3a4cd422b05be713
tl07172 <timlin07172@gmail.com>
2023-09-22 13:14:58 -0400

    Modified hello.py and base.html to reproduce example 4-7

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, EmailField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -14,7 +14,6 @@ moment = Moment(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
-    email = EmailField('What is your UofT Email address?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 
@@ -33,12 +32,8 @@ def index():
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
-        old_email = session.get('email')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
-        if old_email is not None and old_email != form.email.data:
-            flash('Looks like you have changed your email!')
         session['name'] = form.name.data
-        session['email'] = form.email.data
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
+    return render_template('index.html', form=form, name=session.get('name'))
